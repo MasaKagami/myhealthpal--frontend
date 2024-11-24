e74fff8468a606b21ceb754c44aa10a3fc5a736d
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-11-23 21:28:51 -0500

    change

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
           </div>
         </Link>
 
-        <Link href="/local-healthcare" target="_blank" rel="noopener noreferrer">
+        <Link href="/local-healthcare"> {/*target="_blank" rel="noopener noreferrer"*/}
           <div className="btn rounded-full btn-ghost flex justify-center items-center gap-2 text-white border-white bg-myblue">
             Local Healthcare Services
             <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960"  height="24px" width="24px" fill="#e8eaed"><path d="M160-80q-33 0-56.5-23.5T80-160v-480q0-33 23.5-56.5T160-720h160v-80q0-33 23.5-56.5T400-880h160q33 0 56.5 23.5T640-800v80h160q33 0 56.5 23.5T880-640v480q0 33-23.5 56.5T800-80H160Zm0-80h640v-480H160v480Zm240-560h160v-80H400v80ZM160-160v-480 480Zm280-200v120h80v-120h120v-80H520v-120h-80v120H320v80h120Z"/></svg>
